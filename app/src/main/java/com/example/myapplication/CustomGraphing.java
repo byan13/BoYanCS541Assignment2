de4a1ad5b72434e02210c065305fe9187807ea26
Bo Yan <byan2@binghamton.edu>
2019-09-22 14:15:28 -0400

    Version 2.0 updated

--- app/src/main/java/com/example/myapplication/CustomGraphing.java
+++ app/src/main/java/com/example/myapplication/CustomGraphing.java
@@ -13,6 +13,9 @@ public class CustomGraphing extends SurfaceView implements SurfaceHolder.Callbac
     private Paint paint = null;
     private float xCoor = 0;
     private float yCoor = 0;
+    private float radius = 100;
+//    private float x1Coor = 0;
+//    private float y1Coor = 0;
 
     public CustomGraphing(Context context) {
         super(context);
@@ -38,12 +41,25 @@ public class CustomGraphing extends SurfaceView implements SurfaceHolder.Callbac
     public void drawImage(){
         surfaceHolder = getHolder();
         Canvas canvas = surfaceHolder.lockCanvas();
-        Paint surfaceBackground = new Paint();
-        surfaceBackground.setColor(Color.WHITE);
-        canvas.drawRect(0, 0, this.getWidth(), this.getHeight(), surfaceBackground);
-
-        canvas.drawLine(0, 0, getxCoor(), getyCoor(), paint);
-
+        //Paint surfaceBackground = new Paint();
+        //surfaceBackground.setColor(Color.WHITE);
+        //canvas.drawRect(0, 0, this.getWidth(), this.getHeight(), surfaceBackground);
+        canvas.drawColor(Color.WHITE);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setAntiAlias(true);
+        paint.setDither(true);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeJoin(Paint.Join.ROUND);
+        paint.setStrokeCap(Paint.Cap.ROUND);
+        paint.setStrokeWidth(radius);
+        paint.setColor(Color.BLACK);
+        canvas.drawLine(getxCoor(), getyCoor(), 525, 600, paint);
+        paint.setColor(Color.RED);
+        canvas.drawCircle(525, 600, (float)(Math.sqrt(Math.pow(getxCoor() - 525, 2) + Math.pow(getyCoor() - 600, 2))), paint);
+        paint.setColor(Color.GREEN);
+        canvas.drawRect(525 - (float)(Math.sqrt(Math.pow(getxCoor() - 525, 2) + Math.pow(getyCoor() - 600, 2))), 600 - (float)(Math.sqrt(Math.pow(getxCoor() - 525, 2) + Math.pow(getyCoor() - 600, 2))), 525 + (float)(Math.sqrt(Math.pow(getxCoor() - 525, 2) + Math.pow(getyCoor() - 600, 2))), 600 + (float)(Math.sqrt(Math.pow(getxCoor() - 525, 2) + Math.pow(getyCoor() - 600, 2))), paint);
+        paint.setColor(Color.BLUE);
+        canvas.drawRect(525 - (float)(Math.sqrt(2) * (Math.sqrt(Math.pow(getxCoor() - 525, 2) + Math.pow(getyCoor() - 600, 2))) / 2), 600 - (float)(Math.sqrt(2) * (Math.sqrt(Math.pow(getxCoor() - 525, 2) + Math.pow(getyCoor() - 600, 2))) / 2), 525 + (float)(Math.sqrt(2) * (Math.sqrt(Math.pow(getxCoor() - 525, 2) + Math.pow(getyCoor() - 600, 2))) / 2), 600 + (float)(Math.sqrt(2) * (Math.sqrt(Math.pow(getxCoor() - 525, 2) + Math.pow(getyCoor() - 600, 2))) / 2), paint);
         surfaceHolder.unlockCanvasAndPost(canvas);
     }
 
@@ -63,11 +79,42 @@ public class CustomGraphing extends SurfaceView implements SurfaceHolder.Callbac
         this.yCoor = y;
     }
 
+//    public float getx1Coor(){
+//        return x1Coor;
+//    }
+//
+//    public void setx1Coor(float x){
+//        this.x1Coor = x;
+//    }
+//
+//    public float gety1Coor(){
+//        return y1Coor;
+//    }
+//
+//    public void sety1Coor(float y){
+//        this.y1Coor = y;
+//    }
+
     public Paint getPaint(){
         return paint;
     }
 
     public void setPaint(Paint paint){
         this.paint = paint;
+    }
+
+    public float getRadius(){
+        return radius;
+    }
+
+    public void setRadius(float r){
+        this.radius = r;
+    }
+
+    public void resetPaint(){
+        surfaceHolder = getHolder();
+        Canvas canvas = surfaceHolder.lockCanvas();
+        canvas.drawColor(Color.WHITE);
+        surfaceHolder.unlockCanvasAndPost(canvas);
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -16,11 +17,17 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnTouchListener{
 
     private LinearLayout canvasLayout = null;
     CustomGraphing customGraphing = null;
+    SeekBar radiusBar;
+    float slope = 1;
+//    float tempx = 500;
+//    float tempy = 500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +39,32 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         customGraphing = new CustomGraphing(getApplicationContext());
         customGraphing.setOnTouchListener(this);
         canvasLayout.addView(customGraphing);
+
+        ExtendedFloatingActionButton exFab = findViewById(R.id.exFab);
+        exFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                customGraphing.resetPaint();
+            }
+        });
+
+        radiusBar = (SeekBar)findViewById(R.id.radiusbar);
+        radiusBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                customGraphing.setRadius(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(MainActivity.this, "Pen width is " + customGraphing.getRadius(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void initializeControl(){
@@ -46,14 +79,24 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
 
             float x = motionEvent.getX();
             float y = motionEvent.getY();
+//            customGraphing.setxCoor(tempx);
+//            customGraphing.setyCoor(tempy);
+//            customGraphing.setx1Coor(x);
+//            customGraphing.sety1Coor(y);
+//
+//            tempx = x;
+//            tempy = y;
             customGraphing.setxCoor(x);
             customGraphing.setyCoor(y);
 
-            Paint paint = new Paint();
-            paint.setColor(Color.RED);
-            customGraphing.setPaint(paint);
+           //slope = customGraphing.getyCoor() / customGraphing.getxCoor();
 
+            Paint paint = new Paint();
+            //paint.setColor(Color.RED);
+            customGraphing.setPaint(paint);
             customGraphing.drawImage();
+
+            Toast.makeText(MainActivity.this, "x = " + customGraphing.getxCoor() + ", y = " + customGraphing.getyCoor() + "\n" + "Radius = " + (float)(Math.sqrt(Math.pow(customGraphing.getxCoor() - 520, 2) + Math.pow(customGraphing.getyCoor() - 600, 2))), Toast.LENGTH_SHORT).show();
 
             return true;
         }else {
